c5cfe3aeff28055c5dbc2fde88a7366c539c9abe
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-01-20 21:54:44 +0900

    コード整形

--- src/main/scala/jp/_5000164/hugo_url_converter/domain/Converter.scala
+++ src/main/scala/jp/_5000164/hugo_url_converter/domain/Converter.scala
@@ -2,11 +2,8 @@ package jp._5000164.hugo_url_converter.domain
 
 object Converter {
   def toEachDay(basePath: String, filePath: String, content: List[String]): (String, String) = {
-
     def calculate(basePath: String, filePath: String, content: String, remaining: List[String]): (String, String) = {
-
       def buildLineAndDetectFilePath(basePath: String, filePath: String, line: String, content: String): (String, String) = {
-
         def makeFileName(basePath: String, filePath: String, date: String): String = {
           val (year, month, day) = date.split('T').head.split('-') match {
             case Array(y, m, d) => (y, m.filterNot(_ == '0'), d.filterNot(_ == '0'))
