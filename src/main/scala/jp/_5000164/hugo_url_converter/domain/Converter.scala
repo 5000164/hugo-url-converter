72453f9a81e912d0d4410f5dc582122202545c7b
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-01-20 21:38:21 +0900

    変換後のファイルの内容を計算

---
+++ src/main/scala/jp/_5000164/hugo_url_converter/domain/Converter.scala
@@ -0,0 +1,39 @@
+package jp._5000164.hugo_url_converter.domain
+
+object Converter {
+  def toEachDay(basePath: String, filePath: String, content: List[String]): (String, String) = {
+
+    def calculate(basePath: String, filePath: String, content: String, remaining: List[String]): (String, String) = {
+
+      def buildLineAndDetectFilePath(basePath: String, filePath: String, line: String, content: String): (String, String) = {
+
+        def makeFileName(basePath: String, filePath: String, date: String): String = {
+          val (year, month, day) = date.split('T').head.split('-') match {
+            case Array(y, m, d) => (y, m.filterNot(_ == '0'), d.filterNot(_ == '0'))
+          }
+          val fileName = filePath.split('/').last.replaceAll("_", "-")
+          s"$basePath/$year/$month/$day/$fileName"
+        }
+
+        def makeAlias(url: String): String = s"""aliases = ["/$url/"]"""
+
+        val dateRegex = """^date = (.*)$""".r
+        val urlRegex = """^url = "(.*)"$""".r
+        line match {
+          case dateRegex(date) => (makeFileName(basePath, filePath, date), content + "\n" + line)
+          case urlRegex(url) => (filePath, content + "\n" + makeAlias(url))
+          case _ => (filePath, content + "\n" + line)
+        }
+      }
+
+      remaining match {
+        case line :: tail =>
+          val (toFilePath, toContent) = buildLineAndDetectFilePath(basePath, filePath, line, content)
+          calculate(basePath, toFilePath, toContent, tail)
+        case Nil => (filePath, content)
+      }
+    }
+
+    calculate(basePath, filePath, "", content)
+  }
+}
--- src/main/scala/jp/_5000164/hugo_url_converter/interfaces/Application.scala
+++ src/main/scala/jp/_5000164/hugo_url_converter/interfaces/Application.scala
@@ -1,12 +1,15 @@
 package jp._5000164.hugo_url_converter.interfaces
 
+import jp._5000164.hugo_url_converter.domain.Converter
+
 /**
   * アプリケーション起動
   */
 object Application extends App {
-  val targetDirectory = args(0)
-  val files = Load.getTargetFilePathList(targetDirectory)
+  val basePath = args(0)
+  val files = Load.getTargetFilePathList(basePath)
   for (filePath <- files) {
     val content = Load.getContent(filePath)
+    val (toFilePath, toContent) = Converter.toEachDay(basePath, filePath, content)
   }
 }
